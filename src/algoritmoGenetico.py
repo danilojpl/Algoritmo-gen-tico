4bb5e7ae5377ff5eb1a1a48307ed44d6ee1fc960
danilojpl <danilo.duartejp@outlook.com>
2021-04-28 17:25:59 -0300

    fitness

--- src/algoritmoGenetico.py
+++ src/algoritmoGenetico.py
@@ -2,77 +2,138 @@ import random
 import csv 
 
 
-# meta = "h6fAdfSF356D4Gwrd4"
+#constantes
+tabela_itens=[]
+tabela_cidades=[]
 
-# CARACTERES = "abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
 
-def rota_inicial():
+def tabela (local):
     lista = []
-    rota = []
-    with open ('src/itens.csv', encoding = 'utf-8') as itens:
+    with open (local, encoding = 'utf-8') as itens:
         tabela = csv.reader(itens, delimiter=',')
         for l in tabela:
             lista.append(l)
-    for _ in range(4):
-        rota.append(random.choice(lista)) 
+    return lista 
+
+
+def rota_inicial():
+    rota = []
+    lista = tabela('src/itens.csv')
+
+    for _ in range(3):
+        aleatorio = random.choice(lista)
+        rota.append(aleatorio)
+        lista.remove(aleatorio)
     
     return rota 
 
 
 def fitness(rota):
     lucro = 0
-    custo_0 = 0
+    custo = 0
+    peso = 0
+    tempo_gasto = 0
     partida = rota[0]
-    with open ('src/cidades.csv', encoding = 'utf-8') as cidades:
-        tabela = csv.reader(cidades, delimiter=',')
-        for l in tabela:
-            if (l[0]=='Escondidos'):
-                if (partida[4] in l):
-                    custo_0 = l[3]
-            else:
-                break
+    chegada = rota[len(rota)-1]
 
-teste = [['Coroa do Rei João II','5','10','10000','Santa Paula'],['Espada sagrada','6','5','6500','Campos']]
-fitness(teste)
+    tab = tabela_cidades
 
-# def mutar(lista):
-#     nova_lista = list(lista)
-#     novo_digito = random.choice(CARACTERES)
-#     indice = random.randint(0, len(meta) - 1)
-#     nova_lista[indice] = novo_digito
-#     return nova_lista
+    #custo primeira e ultima viagem
+    for l in tab:
+        if (l[0] == 'Escondidos' and partida[4] in l):
+            custo = custo + int(l[3])
+            tempo_gasto = tempo_gasto + int(l[2])
+        if (l[0] == 'Escondidos' and chegada[4] in l):
+            custo = custo + int(l[3])
+            tempo_gasto = tempo_gasto + int(l[2])
 
-# def selecao(lista):
-#     nova_lista = sorted(lista, key=fitness, reverse=True)
-#     return nova_lista[0:10]
-
-# def crossover(populacao, mutada):
-#     populacao_crossover = []
-#     for ind1 in populacao:
-#         for ind2 in mutada:
-#             # geracao do cross_over
-#             i = random.randint(0, len(meta) - 1)
-#             populacao_crossover.append(ind1[0:i] + ind2[i:])
-#             populacao_crossover.append(ind2[0:i] + ind1[i:])
-#     return populacao_crossover
-
-# print('Iniciando...')
-# random.seed()
-
-# # pooulação inicial
-# populacao = [faz_lista_inicial() for _ in range(0,10)]
-
-# geracoes = 0
-# while True:
-#     pop_mutada = [mutar(individuo) for individuo in populacao]
-#     pop_crossover = crossover(populacao, pop_mutada)
-
-#     populacao = selecao(populacao + pop_mutada + pop_crossover)
+    #custo das viagens intermediarias 
+    for i in range(len(rota)):
+        lucro = lucro + int(rota[i][3])
+        peso = peso + int(rota[i][1])
+        tempo_gasto = tempo_gasto + int(rota[i][2])
+        if (i<=len(rota)-2):
+            for l in tab:
+                if (rota[i][4] in l and rota[i+1][4] in l):
+                    custo = custo + int(l[3])
+                    tempo_gasto = tempo_gasto + int(l[2])
     
-#     geracoes += 1
-#     if geracoes % 50 == 0:
-#         print(''.join(populacao[0]), geracoes)
-#     # critério de parada
-#     if fitness(populacao[0]) == len(meta):
-#         break
-# print('Finalizado!')
+    #se passou do peso maximo ou do limite de horas 
+    if (peso > 20 or tempo_gasto > 72):
+        return -1
+    else:
+        return lucro - custo
+                
+    
+def mutar(rota):
+    nova_rota = list(rota)
+
+    indice = random.randint(0, len(rota) - 1)
+    aleatorio = random.choice(tabela_itens)
+    
+    if (aleatorio not in nova_rota):
+        nova_rota[indice] = aleatorio
+    else:
+        index = nova_rota.index(aleatorio)
+        indice = random.randint(0, len(rota) - 1)
+        nova_rota[indice], nova_rota[index] = nova_rota[index], nova_rota[indice]
+
+    if (fitness(nova_rota)<=fitness(rota)):
+        nova_rota.append(random.choice(tabela_itens))
+    
+    if (fitness(nova_rota)==-1):
+        nova_rota.pop()
+
+    return nova_rota
+
+
+def selecao(lista):
+    nova_lista = sorted(lista, key=fitness, reverse=True)
+    return nova_lista[0:10]
+
+
+def crossover(populacao, mutada):
+    populacao_crossover = []
+    for ind1 in populacao:
+        for ind2 in mutada:
+            # geracao do cross_over
+            i = random.randint(0, len(populacao[0]) - 1)
+            if (ind1[0:i] not in ind2[i:]):
+                populacao_crossover.append(ind1[0:i] + ind2[i:])
+                populacao_crossover.append(ind2[0:i] + ind1[i:])
+    return populacao_crossover
+
+
+print('Iniciando...')
+random.seed()
+
+# população inicial
+populacao = [rota_inicial() for _ in range(0,10)]
+print('teste')
+#gerar tabelas 
+tabela_cidades = tabela('src/cidades.csv')
+tabela_itens = tabela('src/itens.csv')
+
+geracoes = 0
+perdas = 0
+ant = -1
+while True:
+    pop_mutada = [mutar(individuo) for individuo in populacao]
+    pop_crossover = crossover(populacao, pop_mutada)
+
+    populacao = selecao(populacao + pop_mutada + pop_crossover)
+    
+    geracoes += 1
+    if geracoes % 50 == 0:
+        print(fitness(populacao[0]), geracoes)
+
+    # critério de parada
+    if fitness(populacao[0]) > ant:
+        ant = fitness(populacao[0])
+        perdas = 0
+    else:
+        perdas+=1
+    if perdas == 500:
+        break
+            
+print('Finalizado!')
